477348ae379902102b30025285e7ebc447fb622b
Bojan Vreco <milosgak2020@gmail.com>
2023-05-04 19:37:12 +0200

    Successfull deployment, attempt 1

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const mongoose = require('mongoose');
 
 const helmet = require('helmet');
 
-const morgan = require('morgan');
-
-const fs = require('fs');
 
 const app = express();
 
@@ -20,15 +17,10 @@ const handleFreeBoxes = require('./postFunctions/free-boxes');
 const handleAboutYou = require('./postFunctions/about-you');
 const handleContact = require('./postFunctions/contact');
 
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
-);
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan('combined', { stream: accessLogStream }));
 
 app.post('/subscribe', handleSubscribe);
 app.post('/pricing', handlePricing);
@@ -36,11 +28,15 @@ app.post('/free-boxes', handleFreeBoxes);
 app.post('/about-you', handleAboutYou);
 app.post('/contact', handleContact);
 
+app.use('/', (req, res) => {
+  res.send('<h1>Hello, World!</h1>');
+});
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongovac.zib5bza.mongodb.net/noworriesmoving?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(process.env.PORT);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => console.log(err));
